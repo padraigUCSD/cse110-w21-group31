ce172bc0761651f2876663dceae3ab7478a61f1e
nmn009 <nmn009@ucsd.edu>
2021-02-17 07:13:46 -0800

    put some code from controller to views

--- source/controllers/pomo-counter-controller.js
+++ source/controllers/pomo-counter-controller.js
@@ -17,31 +17,6 @@ const LONG_BREAK_MIN_LENGTH_SEC = 15 * 60; // 15 minutes
 const LONG_BREAK_MAX_LENGTH_SEC = 30 * 60; // 30 minutes
 
 const POMOS_PER_LONG_BREAK = 4; // number of consecutive pomos before starting a long break
-
-const DOT = 'dot'; //to get the name for the bubble
-var setbackgr = document.body.style.background; // set background color
-const setbubble0 = document.getElementById(DOT + Indicator.ZERO).style.backgroundColor;
-const setbubble1 = document.getElementById(DOT + Indicator.ONE).style.backgroundColor;
-const setbubble2 = document.getElementById(DOT + Indicator.TWO).style.backgroundColor;
-const setbubble3 = document.getElementById(DOT + Indicator.THREE).style.backgroundColor;
-const emptybubble = '#bbb'; //color of normal bubble
-const darkbubble = '#1155cc'; //color of blue bubble
-const startcolor = '#cfe2f3'; //color of light blue for start stage
-const breakcolor = '#d9ead3'; //color of light green for short break stage
-const longcolor = '#b6d7a8'; //color of dark green for long break stage
-
-/**
- * @readonly
- * @enum{int}
- */
-export const Indicator = {
-  ZERO: 0,
-  ONE: 1,
-  TWO: 2,
-  THREE: 3,
-}
-
-
 /**
  * Tracks the current stage of the pomodoro process, and the number of consecutive pomos completed.
  */
@@ -66,18 +41,7 @@ export class PomoCounterController {
     this.timerController.set(POMO_LENGTH_SEC);
 
   }
-  /**
-   * Reset all the bubbles 
-   * Reset the background color
-   * Also it could change the timer display here and button text!!!
-   */
-  reset() {
-    setbubble0 = emptybubble;
-    setbubble1 = emptybubble;
-    setbubble2 = emptybubble;
-    setbubble3 = emptybubble;
-    setbackgr = startcolor;
-  }
+
   /**
    * Advances the cycle to the next stage, i.e. pomo to break or long break,
    * and break or long break to pomo.
@@ -88,27 +52,9 @@ export class PomoCounterController {
       case Stages.POMO:
         if (this.currentPomo === POMOS_PER_LONG_BREAK) {
           this.stage = Stages.LONG_BREAK;
-
-          //change the color for the long break
-          setbackgr = longcolor;
-
           // TODO make long break skippable, set up callback for ending the long break after 30 min
         } else {
           this.stage = Stages.BREAK;
-
-          //change the color for the short break
-          setbackgr = breakcolor;
-          //set the bubbles color base on the currentPomo
-          if (this.currentPomo === Indicator.ZERO){
-            setbubble0 = darkbubble;
-          } else if (this.currentPomo === Indicator.ONE){
-            setbubble1 = darkbubble;
-          } else if (this.currentPomo === Indicator.TWO){
-            setbubble2 = darkbubble;
-          } else {
-            setbubble3 = darkbubble;
-          }
-
           this.timerController.addAlarmCallback('tc_advance', this._advance.call(this));
           this.timerController.set(BREAK_LENGTH_SEC);
         }
@@ -117,10 +63,6 @@ export class PomoCounterController {
       case Stages.BREAK:
         this.stage = Stages.POMO;
         this.currentPomo++;
-
-        //change background color to light blue
-        setbackgr = startcolor;
-
         this.timerController.addAlarmCallback('tc_advance', this._advance.call(this));
         this.timerController.set(POMO_LENGTH_SEC);
         break;
@@ -128,10 +70,6 @@ export class PomoCounterController {
       case Stages.LONG_BREAK:
         this.stage = Stages.POMO;
         this.currentPomo = 0;
-
-        //change background color to light blue and reset the bubbles color
-        this.reset();
-
         this.timerController.addAlarmCallback('tc_advance', this._advance.call(this));
         this.timerController.set(POMO_LENGTH_SEC);
         break;
--- source/index.js
+++ source/index.js
@@ -1,3 +1,4 @@
+import { PomoCounterController } from './controllers/pomo-counter-controller.js';
 import { square } from './square.js';
 
 console.log(`5 squared is ${square(5)}`);
