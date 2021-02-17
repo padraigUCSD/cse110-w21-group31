ebf991b4f49c1d9fa46f87a5ce49db98944a94c7
codefactor-io <support@codefactor.io>
2021-02-17 05:42:44 +0000

    [CodeFactor] Apply fixes

--- source/controllers/pomo-counter-controller.js
+++ source/controllers/pomo-counter-controller.js
@@ -1,110 +1,103 @@
-//timer display
-var second;
-var minute;
-var timeOut;
+// timer display
+let second;
+let minute;
+let timeOut;
 
-var lap = 1; // this variable to keep track what lap we are on
-var skip;
+let lap = 1; // this variable to keep track what lap we are on
+let skip;
 
 document.getElementById("control").addEventListener("click",bubble);
 
-function bubble(){
-	var x = document.getElementById("control").innerHTML;
-	if (x == "Start" || x == "Start Pomo"){
+function bubble() {
+	const x = document.getElementById("control").innerHTML;
+	if (x == "Start" || x == "Start Pomo") {
 		document.body.style.background = "#cfe2f3";
-		minute = 24;			//set minute for display here
-		second = 59;			//set second for display here
+		minute = 24;			// set minute for display here
+		second = 59;			// set second for display here
 		timedCount();
 		StartPomo();
-	} else if (x == "ShortBreak"){
+	} else if (x == "ShortBreak") {
 		document.body.style.background = "#cfe2f3";
-		minute = 4;			//set minute for display here
-		second = 29;			//set second for display here
+		minute = 4;			// set minute for display here
+		second = 29;			// set second for display here
 		timedCount();
 		ShortBreak();
-	} else if (x == "LongBreak"){
-		minute = 29;			//set minute for display here
-		second = 59;			//set second for display here
+	} else if (x == "LongBreak") {
+		minute = 29;			// set minute for display here
+		second = 59;			// set second for display here
 		timedCount();
 		LongBreak();
-	} else if (x == "Skip"){
+	} else if (x == "Skip") {
 		Skip();
-	}	
+	}
 }
-function StartPomo(){
+function StartPomo() {
 	document.getElementById("control").style.display = "none";
 	document.body.style.background = "#cfe2f3";
-	if (lap == "4"){
-		document.getElementById("control").innerHTML= "LongBreak";
+	if (lap == "4") {
+		document.getElementById("control").innerHTML = "LongBreak";
 	} else {
-		document.getElementById("control").innerHTML= "ShortBreak";
+		document.getElementById("control").innerHTML = "ShortBreak";
 	}
-	setTimeout(function(){
-		document.getElementById("dot" + lap).style.backgroundColor = "#1155cc";
-		document.getElementById("control").style.display = "block";	
+	setTimeout(() => {
+		document.getElementById(`dot${lap}`).style.backgroundColor = "#1155cc";
+		document.getElementById("control").style.display = "block";
 		lap = parseInt(lap);
-		lap = lap + 1;
-		
-	}, 1500000);	
+		lap += 1;
+	}, 1500000);
 }
-function ShortBreak(){
+function ShortBreak() {
 	document.getElementById("control").style.display = "none";
 	document.body.style.background = "#d9ead3";
-	setTimeout(function(){
-		
+	setTimeout(() => {
 		document.getElementById("control").style.display = "block";
-		document.getElementById("control").innerHTML = "Start Pomo";	
-		
-		
+		document.getElementById("control").innerHTML = "Start Pomo";
 	}, 300000);
 }
 
-function Skip(){
+function Skip() {
 	clearTimeout(skip);
 	clearTimeout(timeOut);
-	clear();	
+	clear();
 }
-function LongBreak(){
+function LongBreak() {
 	document.getElementById("control").style.display = "none";
 	document.body.style.background = "#b6d7a8";
-	setTimeout(function(){
+	setTimeout(() => {
 		document.getElementById("control").style.display = "block";
 		document.getElementById("control").innerHTML = "Skip";
-	}, 900000);			//half of the long break is 15' * 60" * 1000 = 900000
-	
-	skip = setTimeout(function(){	
+	}, 900000);			// half of the long break is 15' * 60" * 1000 = 900000
+
+	skip = setTimeout(() => {
 		clear();
-	}, 1800000);			//long break is 30' * 60" * 1000 = 1800000
-	
+	}, 1800000);			// long break is 30' * 60" * 1000 = 1800000
 }
-function clear(){
+function clear() {
 	lap = 1;
-	for (let i = 1; i < 5; i ++){
-		document.getElementById("dot" + i).style.backgroundColor = "#bbb";
+	for (let i = 1; i < 5; i++) {
+		document.getElementById(`dot${i}`).style.backgroundColor = "#bbb";
 	}
-	document.getElementById("control").innerHTML= "Start";
+	document.getElementById("control").innerHTML = "Start";
 	document.body.style.background = "#cfe2f3";
 	document.getElementById("counter").innerHTML = "25:00";
 }
 
-
 function timedCount() {
-  if (second < 10 && minute < 10){
-  	document.getElementById("counter").innerHTML = "0" + minute + ":0" + second;
-  } else if (second < 10){
-  	document.getElementById("counter").innerHTML = minute + ":0" + second;
+  if (second < 10 && minute < 10) {
+  	document.getElementById("counter").innerHTML = `0${minute}:0${second}`;
+  } else if (second < 10) {
+  	document.getElementById("counter").innerHTML = `${minute}:0${second}`;
   } else if (minute < 10) {
-  	document.getElementById("counter").innerHTML = "0" + minute + ":" + second;
+  	document.getElementById("counter").innerHTML = `0${minute}:${second}`;
   } else {
-  	document.getElementById("counter").innerHTML = minute + ":" + second;
+  	document.getElementById("counter").innerHTML = `${minute}:${second}`;
   }
-  second = second - 1;
-  if (second < 0){
-  	minute = minute - 1;
+  second -= 1;
+  if (second < 0) {
+  	minute -= 1;
     second = 59;
   }
-  if (minute >= 0){
+  if (minute >= 0) {
   	timeOut = setTimeout(timedCount, 1000);
   }
 }
-
