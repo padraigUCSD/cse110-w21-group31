a45da25c3e2b565bfb7d418fb19f38baa1df6be1
Stephen <stephenwang51924@gmail.com>
2021-02-21 22:33:35 -0800

    ctor and advance test

--- source/controllers/pomo-counter-controller.test.js
+++ source/controllers/pomo-counter-controller.test.js
@@ -9,23 +9,53 @@ test('Counter starts in a pomo', () => {
   expect(counter._stage).toBe(Stages.POMO);
 })
 
+test('constructor works properly', () => {
+  // Assertions
+  expect(counter._timerController).toBe(timer); //hmm
+  expect(counter._stage).toBe(Stages.POMO);
+  expect(counter._currentPomo).toBe(Number(1));
+  expect(counter._skippable).toBe(false);
+  //expect(counter._skippableCallbacks).toBe({});
+})
+
 test('Counter advances to a break', () => {
   // Setup
   counter._advance();
 
   // Assertions
   expect(counter._stage).toBe(Stages.BREAK);
+
+  //should be 1 because we have not started the 2nd pomo yet
+  //todo
+
+
+})
+
+test('Counter advances to 2nd pomo', () => {
+  //Setup 
+  counter._advance();
+
+  //Assertions
+  expect(counter.stage).toBe(Stages.POMO);
+
+  //should be 2 now that we have advances
+  //todo
+
 })
 
 test('After 4 pomos, transitions to a longer break', () => {
   // Setup
-  const numAdvances = 6;
+  const numAdvances = 5; //6;
   for (let i = 0; i < numAdvances; i++) {
     counter._advance();
   }
 
   // Assertions
   expect(counter._stage).toBe(Stages.LONG_BREAK);
+  //testing that all traits of a long break are true
+  expect(counter._currentPomo).toBe(Number(1));
+
+  
 })
 
 
