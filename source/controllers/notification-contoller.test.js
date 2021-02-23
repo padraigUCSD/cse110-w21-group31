ea5b7308eae9f71873056dddcbbffd0deb8baf02
dkhamilt <dkhamilt@ucsd.edu>
2021-02-23 12:35:13 -0800

    Notification controller start

---
+++ source/controllers/notification-contoller.test.js
@@ -0,0 +1,8 @@
+import { NotificationController } from '.notification-controller.js';
+import { PomoCounterController, Stages } from './pomo-counter-controller.js';
+//Need notification controller
+const counter = new PomoCounterController(timer);
+test('Sound Plays', () => {
+    // Assertions
+    counter.alarm.play();
+  })
