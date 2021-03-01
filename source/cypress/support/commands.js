4f1614ec06369f734d9ed6df8ae89077bce14fc2
David Cruz <david@typokign.com>
2021-02-28 17:17:58 -0800

    Set up Cypress and add some tests

---
+++ source/cypress/support/commands.js
@@ -0,0 +1,13 @@
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+addMatchImageSnapshotCommand();
+
+/**
+ * Add any commands to run before each spec here. Call cy.prepare() in the before() section of any spec.
+ *
+ * Currently cy.visit()'s the URL specified in process.env.URL, or localhost:8080 if unspecified.
+ */
+Cypress.Commands.add('prepare', () => {
+  const url = process.env.URL || 'http://localhost:8080';
+  cy.visit(url);
+})
