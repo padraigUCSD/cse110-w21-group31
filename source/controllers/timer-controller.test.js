c1f4baf68cc67486bdb2c4b4611cc50cb95680fe
nmn009 <nmn009@ucsd.edu>
2021-03-04 16:09:50 -0800

    update test

--- source/controllers/timer-controller.test.js
+++ source/controllers/timer-controller.test.js
@@ -1,4 +1,4 @@
-import { jest } from '@jest/globals';
+import { expect, jest } from '@jest/globals';
 import { TimerController } from './timer-controller.js';
 
 const sleep = async (ms) => {
@@ -26,9 +26,16 @@ test('Timer can count to 10 seconds', async () => {
 test('check alarm call back in 2s', async () => {
   // Setup
   const timer = new TimerController();
-  const alarm = jest.fn();
-  timer.addAlarmCallback('test', alarm);
-  expect(alarm).toBeCalled();
+  let checker = 0;
+  timer.addAlarmCallback('test', () => {
+    checker = 2;
+  });
+  timer.set(2);
+  
+  expect(checker).toBe(0);
+
+  await sleep(2_010);
+  expect(checker).toBe(2);
 });
 
 test('check constructor correctly', () => {
