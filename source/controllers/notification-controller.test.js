119b3de74452bf3d40537657e9b34fddf1ee723a
Stephen <stephenwang51924@gmail.com>
2021-03-07 12:52:06 -0800

    reorganized/fixed notification-controller.test.js

--- source/controllers/notification-controller.test.js
+++ source/controllers/notification-controller.test.js
@@ -2,21 +2,29 @@ import { NotificationController } from './notification-controller.js';
 
 const notif = new NotificationController();
 
-describe('test constructor and setSound function', () => {
+describe('tests constructor', () => {
     test('constructor', () => {
         expect(notif._sound).toBe('normal');
     });
+});
 
-    test('setSound', () => {
+describe('tests setSound', () => {
+    test('set sound to normal', () => {
         notif.setSound('normal');
         expect(notif._sound).toBe('normal');
+    });
 
+    test('set sound to alt', () => {
         notif.setSound('alt');
         expect(notif._sound).toBe('alt');
+    });
 
+    test('set sound to screaming', () => {
         notif.setSound('screaming');
         expect(notif._sound).toBe('screaming');
+    });
 
+    test('set sound to error', () => {
         notif.setSound('error sound');
         expect(notif._sound).toBe('error sound');
         notif.setSound(notif.playSound());
@@ -24,8 +32,7 @@ describe('test constructor and setSound function', () => {
     });
 });
 
-describe('tests playSound()', () => {
-
+describe('tests playSound', () => {
     test('test case Sounds.NORMAL', ()=> {
         notif.setSound('normal');
         let sound = notif.playSound();
@@ -38,7 +45,7 @@ describe('tests playSound()', () => {
         expect(sound).toBe('alt');
     });
 
-    test('test case Sounds.CREAMING', () => {
+    test('test case Sounds.SCREAMING', () => {
         notif.setSound('screaming');
         const sound = notif.playSound();
         expect(sound).toBe('screaming');
